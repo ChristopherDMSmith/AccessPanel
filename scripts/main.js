7af5d9aef9dba2decf4a9c11af5fb7c72792262f
Christopher David Michael Smith <chris.dm.smith@gmail.com>
2025-12-14 18:43:36 -0500

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -545,7 +545,7 @@ function themeSelection(event) {
 // Restore the last selected theme on load
 async function restoreSelectedTheme() {
   chrome.storage.local.get("selectedTheme", async (result) => {
-    const themeKey = result.selectedTheme || "Workforce MGR"; // default theme
+    const themeKey = result.selectedTheme || "Access Panel";
     await applyTheme(themeKey);
 
     const dropdown = document.getElementById("theme-selector");
